a0ef33c4f10c25f89fac39b3af51a15684a807d6
AlkarimJ1997 <ajiwa97@gmail.com>
2023-07-24 14:39:00 -0700

    Temp commit

--- data/maps/DemoRoom.ts
+++ data/maps/DemoRoom.ts
@@ -3,13 +3,9 @@ import { MAPS, SKINS, PLACEMENT_TYPES } from '@/utils/consts';
 const OVERWORLD_STATE: OverworldConfig = {
 	map: MAPS.DemoRoom,
 	placements: {
-		hero: {
-			x: 5,
-			y: 6,
-			skin: SKINS.HERO,
-			type: PLACEMENT_TYPES.HERO,
-		},
+		hero: { x: 5, y: 5, skin: SKINS.HERO, type: PLACEMENT_TYPES.HERO },
 		npcA: { x: 7, y: 8, skin: SKINS.NPC1 },
+    npcB: { x: 3, y: 6, skin: SKINS.NPC2 },
 	},
 };
 
