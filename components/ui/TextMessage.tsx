ca2c00cbd3c87315160b22f527efa5a7bd8004ec
Alkarim Jiwa <ajiwa10@my.bcit.ca>
2023-07-31 23:57:35 -0700

    Updated message styles

--- components/ui/TextMessage.tsx
+++ components/ui/TextMessage.tsx
@@ -12,9 +12,9 @@ const TextMessage = ({ overworld }: TextMessageProps) => {
 	return (
 		<div
 			className={clsx(
-				'py-1 px-2 rounded-md bg-slate-700 text-slate-100 shadow-xl border-2 border-indigo-400 absolute bottom-16 w-[40vw]',
-				overworld.battle && 'min-h-[15vh] text-lg',
-				!overworld.battle && 'min-h-[15vh] text-lg'
+				'py-1 px-2 rounded-md bg-slate-700 text-slate-100 shadow-xl border-2 border-indigo-400 absolute bottom-16 text-sm sm:text-lg md:text-xl',
+				overworld.battle && '',
+				!overworld.battle && 'min-h-[15vh] w-[90%] max-w-lg'
 			)}>
 			{characters.map(({ char, show }, i) => (
 				<span key={i} className={clsx(show ? 'opacity-100' : 'opacity-0')}>
@@ -23,7 +23,7 @@ const TextMessage = ({ overworld }: TextMessageProps) => {
 			))}
 			<div
 				className={clsx(
-					'absolute bottom-1 right-2 text-4xl leading-none',
+					'absolute bottom-1 right-2 text-2xl leading-none md:text-4xl',
 					overworld.battle && 'hidden'
 				)}>
 				{['.', '.', '.'].map((char, i) => (
