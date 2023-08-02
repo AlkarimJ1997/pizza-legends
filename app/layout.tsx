510b38645fd9c3198852a50ff73523c366e58aaf
Alkarim Jiwa <ajiwa10@my.bcit.ca>
2023-08-02 00:52:21 -0700

    Updated fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,20 @@
 import './globals.css';
-import { Electrolize, Changa, VT323 } from 'next/font/google';
+import {
+	Electrolize,
+	Changa,
+	VT323,
+	Ubuntu_Mono,
+	Syne_Mono,
+	Nova_Mono,
+} from 'next/font/google';
 import localFont from 'next/font/local';
 
-// const primary = Electrolize({
-// 	weight: ['400'],
-// 	subsets: ['latin'],
-// });
+// const primary = localFont({ src: '../fonts/BadComic-Regular.woff2' });
 
-// const primary = VT323({
-// 	weight: ['400'],
-// 	subsets: ['latin'],
-// });
-
-const primary = localFont({ src: '../fonts/BadComic-Regular.woff2' });
+const primary = Syne_Mono({
+	weight: ['400'],
+	subsets: ['latin'],
+});
 
 export const metadata = {
 	title: 'Pizza Legends',
