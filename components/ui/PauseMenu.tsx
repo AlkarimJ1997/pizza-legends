66860504d12fe8f896627cde06853d7fe31cbbee
Alkarim Jiwa <ajiwa10@my.bcit.ca>
2023-08-08 13:16:39 -0700

    Basic styles for pause menu

--- components/ui/PauseMenu.tsx
+++ components/ui/PauseMenu.tsx
@@ -1,17 +1,44 @@
-import Menu from '@/components/battle/BattleMenu';
+import useMenu from '@/hooks/useMenu';
+import clsx from 'clsx';
 
 interface PauseMenuProps {
 	overworld: OverworldChanges;
 }
 
 const PauseMenu = ({ overworld }: PauseMenuProps) => {
+	const options = overworld.pause?.keyboardMenu?.options ?? [];
+	const { focusedIndex, buttonRefs, handleFocus } = useMenu({ options });
+
 	if (!overworld.pause) return null;
 
-	const options = overworld.pause.keyboardMenu?.options ?? [];
-
 	return (
-		<div className='absolute inset-0 grid place-items-center'>
-			{/* <Menu options={options} inBattle={false} /> */}
+		<div className='absolute inset-0 z-50 grid place-items-center auto-rows-fr'>
+			<div className='bg-slate-700 text-slate-100 w-[90%] mx-auto max-w-md p-4 rounded-md shadow-xl'>
+				<h2 className='mb-4 text-3xl text-center border-b border-b-slate-400'>
+					Pause Menu
+				</h2>
+				{options.map(({ label, disabled, handler, right }, i) => (
+					<div key={i} className='text-lg md:text-2xl'>
+						<button
+							ref={el => (buttonRefs.current[i] = el)}
+							disabled={!!disabled}
+							onClick={handler}
+							onMouseEnter={event => handleFocus(event, i)}
+							className={clsx(
+								'flex justify-between items-center w-full cursor-pointer focus:outline-none focus:ring-2 focus:ring-indigo-500 px-4 py-2',
+								disabled && 'opacity-50'
+							)}>
+							{label}
+							<span className='text-slate-300'>{right?.()}</span>
+						</button>
+					</div>
+				))}
+			</div>
+			<div className='w-[90%] max-w-xl px-4 py-2 mx-auto ring-2 ring-indigo-500 rounded-sm bg-slate-700 text-slate-100'>
+				<p className='text-lg md:text-xl'>
+					{options[focusedIndex]?.description}
+				</p>
+			</div>
 		</div>
 	);
 };
